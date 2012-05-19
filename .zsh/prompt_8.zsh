919bae81f5d0e1a4231351feff508a60c27acc6a
Daniel Beecham <daniel@lunix.se>
2012-05-19 15:04:20 +0200

    White CWD instead of black works better on dark screens.

--- .zsh/prompt_8.zsh
+++ .zsh/prompt_8.zsh
@@ -1,6 +1,6 @@
 PROMPT_ROOT="%{$fg_bold[black]%}<%{$fg_bold[yellow]%}root%{$fg_bold[black]%}>"
-PROMPT_HOSTNAME="%{$fg_bold[black]%}(%{$fg_bold[yellow]%}HOSTNAME%{$fg_bold[black]%})"
+PROMPT_HOSTNAME="%{$fg_bold[black]%}(%{$fg_bold[yellow]%}infect%{$fg_bold[black]%})"
 PROMPT_ERROR="%(?.. %{$fg_bold[black]%}[%{$fg_bold[red]%}%?%{$fg_bold[black]%}])"
-PROMPT_CWD="%{$fg_bold[black]%}[%30<...<%~]"
+PROMPT_CWD="%{$fg_bold[black]%}[$fg_bold[white]%30<...<%~$fg_bold[black]]"
 PROMPT_ENDBRACKET="%{$fg_bold[yellow]%}>"
 PROMPT="$PROMPT_ERROR $PROMPT_HOSTNAME $PROMPT_CWD $PROMPT_ENDBRACKET %{$reset_color%}"
