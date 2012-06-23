35c0bd56bceae141fe6e43619971831d3069b1cc
Daniel Beecham <daniel@lunix.se>
2012-06-23 14:25:54 +0200

    Prompt fix, pacman-color alias, new kernel configuration for desktop.
    --HG--
    rename : .config => laptop.config

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,16 +1,13 @@
 # Common commands and aliases.
 alias ls="ls --color -F"
-alias 1="ssh 10.1"
 alias lsof="lsof -Pn"
 alias mmv='noglob zmv -W'
-alias waf='CXX=/usr/bin/g++ CC=/usr/bin/gcc waf'
 alias -g ...="../.."
 alias -g ....="../../.."
 alias -g .....="../../../.."
-alias fehh="feh -d -g 640x480"
 alias pgr="vimpager"
 alias rm="rm -v"
-alias lipt="iptables -nvL"
 alias mv="mv -v"
 alias ds="dirs -v"
 alias ps='ps af -o pid,user,tty,command'
+alias pc="pacman-color"
--- .zsh/prompt_8.zsh
+++ .zsh/prompt_8.zsh
@@ -1,6 +1,11 @@
 PROMPT_ROOT="%{$fg_bold[black]%}<%{$fg_bold[yellow]%}root%{$fg_bold[black]%}>"
-PROMPT_HOSTNAME="%{$fg_bold[black]%}(%{$fg_bold[yellow]%}inject%{$fg_bold[black]%})"
+PROMPT_HOSTNAME="%{$fg_bold[black]%}(%{$fg_bold[yellow]%}%m%{$fg_bold[black]%})"
 PROMPT_ERROR="%(?.. %{$fg_bold[black]%}[%{$fg_bold[red]%}%?%{$fg_bold[black]%}])"
 PROMPT_CWD="%{$fg_bold[black]%}[%{$fg_bold[white]%}%30<...<%~%{$fg_bold[black]%}]"
 PROMPT_ENDBRACKET="%{$fg_bold[yellow]%}>"
-PROMPT="$PROMPT_ERROR $PROMPT_HOSTNAME $PROMPT_CWD $PROMPT_ENDBRACKET %{$reset_color%}"
+
+if [[ `whoami` == "root" ]]; then
+	PROMPT="$PROMPT_ERROR $PROMPT_ROOT $PROMPT_HOSTNAME $PROMPT_CWD $PROMPT_ENDBRACKET %{$reset_color%}"
+else
+	PROMPT="$PROMPT_ERROR $PROMPT_HOSTNAME $PROMPT_CWD $PROMPT_ENDBRACKET %{$reset_color%}"
+fi
