058fa6691432ece2198d2f73c409c521dec82a7e
Daniel Beecham <daniel@lunix.se>
2012-10-12 19:44:22 +0000

    Added package manager aliases.

--- .bash/aliases.bash
+++ .bash/aliases.bash
@@ -3,3 +3,21 @@ alias lsof="lsof -Pn"
 alias rm="rm -v"
 alias mv="mv -v"
 alias ps="ps af -o pid,user,tty,command"
+
+if test $DIST == "arch"; then
+    alias add="packer -S"
+    alias del="$PACMAN -Rs"
+    alias search="packer -Ss"
+    alias info="packer -Si"
+elif test $DIST == "gentoo"; then
+    alias add="emerge"
+    alias del="emerge --unmerge"
+    alias search="qsearch"
+    alias info="emerge --info"
+elif test $DIST == "alpine"; then
+    alias add="apk add"
+    alias del="apk del"
+    alias search="apk search"
+    alias info="apk info"
+    # IS ALPINE AWESOME OR WHAT?!
+fi
--- .bash/environment.bash
+++ .bash/environment.bash
@@ -2,3 +2,14 @@ export LANG="en_US.UTF-8"
 export EDITOR="/usr/bin/vim"
 export PAGER="/usr/bin/less"
 export LESS="-R"
+export PACMAN="/usr/bin/pacman-color"
+
+if test -e /etc/arch-release; then
+    export DIST="arch"
+elif test -e /etc/alpine-release; then
+    export DIST="alpine"
+elif test -e /etc/gentoo-release; then
+    export DIST="gentoo"
+else
+    export DIST="custom"
+fi
