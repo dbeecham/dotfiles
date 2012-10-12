c5cf3ae435049175677c9923247f2be068b36d57
Daniel Beecham <daniel@lunix.se>
2012-10-12 19:54:15 +0000

    Bug in short_pwd(), ugly fix.

--- .bash/functions.bash
+++ .bash/functions.bash
@@ -9,10 +9,10 @@ function cd() {
 short_pwd() {
     cwd=$(pwd)
 
-    if [ $cwd == $HOME ]; then echo -n "~"; return; fi
-    if [ $cwd == "/" ]; then echo -n "/"; fi
+    if test "$cwd" == "$HOME"; then echo -n "~"; return; fi
+    if test "$cwd" == "/"; then echo -n "/"; fi
 
-    for l in $(echo $cwd | tr "/" "\n"); do
+    for l in $(echo $cwd | tr " " "_" | tr "/" "\n"); do
         echo -n "/"
         echo -n ${l:0:1}
     done
