960736f7acd74cb011be4758afac40304222558b
Daniel Beecham <daniel@lunix.se>
2014-10-26 15:46:38 +0100

    la alias in zsh

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,5 +1,6 @@
 # Common commands and aliases.
 alias ls="ls --color -F --group-directories-first"
+alias la="ls -a --color -F --group-directories-first"
 alias lsof="lsof -Pn"
 alias mmv='noglob zmv -W'
 alias -g ...="../.."
