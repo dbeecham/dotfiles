44c4598164d3cbe7a33f9dd0bef22dd0f49c6f2e
Daniel Beecham <daniel@lunix.se>
2014-10-26 14:23:02 +0100

    ZSH prompt fixed.

--- .zsh/prompt_8.zsh
+++ .zsh/prompt_8.zsh
@@ -1,16 +1,14 @@
-PROMPT_ROOT="%{$fg_bold[red]%}@"
-PROMPT_ERROR="%(?..%{$fg_bold[white]%}[%{$fg_bold[red]%}%?%{$fg_bold[white]%}])"
-PROMPT_CWD="%{$fg_bold[white]%}%30<...<%~"
-PROMPT_ENDBRACKET="%{$fg_bold[white]%}%#"
+PROMPT_ROOT="%{$fg_bold[red]%}@%{$reset_color%}"
+PROMPT_ERROR="%(?..[%{$fg_bold[red]%}%?%{$reset_color%}])"
+PROMPT_CWD="%30<...<%~%<<"
+PROMPT_ENDBRACKET="%#"
 
-HOST_IDENTIFIER="%{$fg_bold[blue]%}#"
+HOST_IDENTIFIER="%{$fg_bold[blue]%}#%{$reset_color%}"
 
 if [[ `whoami` == "root" ]]; then
     PROMPT="
-$PROMPT_ERROR%{$fg_bold[white]%}($PROMPT_ROOT$HOST_IDENTIFIER%{$fg_bold[white]%}) $PROMPT_CWD $PROMPT_ENDBRACKET "
-#   PROMPT="$PROMPT_ERROR $PROMPT_ROOT $PROMPT_HOSTNAME $PROMPT_CWD $PROMPT_ENDBRACKET %{$reset_color%}"
+$PROMPT_ERROR($PROMPT_ROOT$HOST_IDENTIFIER) $PROMPT_CWD $PROMPT_ENDBRACKET "
 else
     PROMPT="
-$PROMPT_ERROR%{$fg_bold[white]%}($HOST_IDENTIFIER%{$fg_bold[white]%}) $PROMPT_CWD $PROMPT_ENDBRACKET "
-    #PROMPT="$PROMPT_ERROR $PROMPT_HOSTNAME $PROMPT_CWD $PROMPT_ENDBRACKET %{$reset_color%}"
+$PROMPT_ERROR($HOST_IDENTIFIER) $PROMPT_CWD $PROMPT_ENDBRACKET "
 fi
