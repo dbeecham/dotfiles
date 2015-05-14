a28913aa86584d817613d1c6aa8043020ce59e8b
Daniel Beecham <daniel@lunix.se>
2015-05-14 11:25:36 +0200

    ghc-mod temporary in path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,11 @@
 set fish_greeting ""
 
-set PATH ~/.nix-profile/bin /usr/local/bin $PATH
+set PATH $HOME/.nix-profile/bin /usr/local/bin $HOME/.cabal/bin $PATH $HOME/bin/
+set PATH /opt/ghc-mod/dist/build/ghc-mod /opt/ghc-mod/dist/build/ghc-modi/ $PATH 
 
 function chpwd --on-variable PWD
     status --is-command-substitution; and return
     ls
 end
+
+set -xg DARK (cat ~/.dark)
