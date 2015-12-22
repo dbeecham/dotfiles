c8a3c13239c3a5aa1e5ad1e2e31789ff3a44b157
Daniel Beecham <daniel@lunix.se>
2015-12-22 20:42:05 +0100

    Fish vim mode

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,7 @@
 set fish_greeting ""
 
+fish_vi_mode
+
 set PATH $HOME/.nix-profile/bin /usr/local/bin $HOME/.cabal/bin $PATH $HOME/bin/
 set PATH /opt/ghc-mod/dist/build/ghc-mod /opt/ghc-mod/dist/build/ghc-modi/ $PATH 
 
