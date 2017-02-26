212409e914afcefc4f02626681a52a6ca6b8409f
Daniel Beecham <daniel@lunix.se>
2017-02-26 10:57:11 +0100

    bash: zfz to use ag instead of find; added nix config

--- .bash/environment.bash
+++ .bash/environment.bash
@@ -6,3 +6,14 @@ export LESS="-R"
 if test $TERM = "rxvt-unicode"; then
     export TERM="xterm-256color"
 fi
+
+# If nix is installed, source it.
+if test -e ~/.nix-profile/etc/profile.d/nix.sh; then
+    source ~/.nix-profile/etc/profile.d/nix.sh
+fi
+
+# I like when FZF picks up on .ignore, .gitignore, etc.
+# It's only a couple of environment variables anyway.
+export FZF_DEFAULT_COMMAND='ag -g ""'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
