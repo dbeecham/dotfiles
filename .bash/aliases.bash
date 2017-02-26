4b6e1e58dbf8890b680c572e3d9f2b6f8a4b0e01
Daniel Beecham <daniel@lunix.se>
2017-02-26 10:58:48 +0100

    bash: use trash instead of rm

--- .bash/aliases.bash
+++ .bash/aliases.bash
@@ -1,6 +1,6 @@
 alias ls="ls --color -F --group-directories-first"
 alias lsof="lsof -Pn"
-alias rm="rm -v"
+alias rm="trash -v"
 alias mv="mv -v"
 alias ps="ps af -o pid,user,tty,command"
 alias g="git"
