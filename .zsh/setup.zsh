095322ddbc1ee5f84a3ce2722fcd9a85f29d7bac
Daniel Beecham <daniel@lunix.se>
2017-08-08 10:27:36 +0200

    zsh: enable 'zsh-completions' plugin
    This needs documentation.

--- .zsh/setup.zsh
+++ .zsh/setup.zsh
@@ -5,6 +5,7 @@ antigen use oh-my-zsh
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle git
 antigen bundle pip
+antigen bundle zsh-users/zsh-completions
 antigen theme fishy
 
 antigen apply
