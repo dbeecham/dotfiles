b11a6ce9ba84bc5c03ef35737466162919aa0e82
Daniel Beecham <daniel@lunix.se>
2018-03-04 10:49:44 +0000

    awesome: use qterminal, super meta key, add xbacklight

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -44,7 +44,7 @@ end
 beautiful.init("/home/dbe/.config/awesome/themes/zenburn/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "urxvt"
+terminal = "qterminal"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
@@ -53,7 +53,7 @@ editor_cmd = terminal .. " -e " .. editor
 -- If you do not like this or do not have such a key,
 -- I suggest you to remap Mod4 to another key using xmodmap or other tools.
 -- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod1"
+modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
@@ -332,6 +332,8 @@ globalkeys = gears.table.join(
 
     awful.key({}, "XF86AudioRaiseVolume", function() awful.util.spawn("amixer set Master 2+") end),
     awful.key({}, "XF86AudioLowerVolume", function() awful.util.spawn("amixer set Master 2-") end),
+    awful.key({}, "XF86MonBrightnessUp", function() awful.util.spawn("xbacklight +10") end),
+    awful.key({}, "XF86MonBrightnessDown", function() awful.util.spawn("xbacklight -10") end),
 
     awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)          end,
               {description = "increase master width factor", group = "layout"}),
