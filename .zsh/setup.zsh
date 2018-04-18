c6da490e7e05f1788e42379c43709abf5065c51f
Daniel Beecham <daniel@lunix.se>
2018-04-18 10:16:56 +0200

    zsh: disable zsh-nix-shell plugin

--- .zsh/setup.zsh
+++ .zsh/setup.zsh
@@ -6,7 +6,10 @@ zplug "urbainvaes/fzf-marks"
 zplug "Tarrasch/zsh-functional"
 zplug "zsh-users/zsh-autosuggestions"
 zplug "ytet5uy4/fzf-widgets"
-zplug "chisui/zsh-nix-shell"
+
+# this plugin is apparently pretty buggy - so I disabled it.
+#zplug "chisui/zsh-nix-shell"
+
 zplug "hcgraf/zsh-sudo"
 zplug "clvv/fasd"
 # antigen theme fishy
