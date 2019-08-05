a59067cadd034d330672db1457ea96cf67764fb9
Daniel Beecham <daniel@lunix.se>
2019-08-05 13:18:21 +0200

    remove zsh widgets

--- zsh/.zsh/widgets.zsh
+++ zsh/.zsh/widgets.zsh
@@ -1,21 +1,24 @@
-function _go_home {
-    if test "$BUFFER" = ""; then
-        echo
-        cd
-    else
-        BUFFER="${BUFFER}gh"
-        CURSOR=$(($CURSOR+2))
-    fi
-}
-zle -N _go_home
-bindkey 'gh' _go_home
-
-function _mycd {
-    if test "$BUFFER" = ""; then
-        echo "abc";
-    else
-        echo "def";
-    fi
-}
-zle -N _mycd
-bindkey ',g' _mycd
+# I never use these, and they just get in the way. They are cool though, and I might
+# improve them in the future.
+#
+#function _go_home {
+#    if test "$BUFFER" = ""; then
+#        echo
+#        cd
+#    else
+#        BUFFER="${BUFFER}gh"
+#        CURSOR=$(($CURSOR+2))
+#    fi
+#}
+#zle -N _go_home
+#bindkey 'gh' _go_home
+#
+#function _mycd {
+#    if test "$BUFFER" = ""; then
+#        echo "abc";
+#    else
+#        echo "def";
+#    fi
+#}
+#zle -N _mycd
+#bindkey ',g' _mycd
