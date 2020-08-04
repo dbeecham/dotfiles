4b22f2979e2059c827078edcc3a8403a9ae580a6
Daniel Beecham <daniel@lunix.se>
2020-08-04 13:36:12 +0200

    add SSH_AUTH_SOCK to fix-ssh-agent

--- bin/bin/fix-ssh-agent.sh
+++ bin/bin/fix-ssh-agent.sh
@@ -3,5 +3,5 @@
 OPENSC_LIB="$(nix-store --realise $(nix-instantiate --expr '(import <nixpkgs> {}).opensc' 2>/dev/null) 2>/dev/null)/lib/opensc-pkcs11.so"
 
 pkill ssh-agent
-eval $(ssh-agent -P"${OPENSC_LIB}")
+eval $(ssh-agent -a"${SSH_AUTH_SOCK}" -P"${OPENSC_LIB}")
 ssh-add -s "${OPENSC_LIB}"
