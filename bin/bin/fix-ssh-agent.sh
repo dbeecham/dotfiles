14ed6218cc5c32bcc3b736b9bfd73428e4df5cfe
Daniel Beecham <daniel@lunix.se>
2020-08-04 13:44:05 +0200

    kill gpg-agent with fix-ssh-agent

--- bin/bin/fix-ssh-agent.sh
+++ bin/bin/fix-ssh-agent.sh
@@ -2,6 +2,7 @@
 
 OPENSC_LIB="$(nix-store --realise $(nix-instantiate --expr '(import <nixpkgs> {}).opensc' 2>/dev/null) 2>/dev/null)/lib/opensc-pkcs11.so"
 
+pkill gpg-agent
 pkill ssh-agent
 eval $(ssh-agent -a"${SSH_AUTH_SOCK}" -P"${OPENSC_LIB}")
 ssh-add -s "${OPENSC_LIB}"
