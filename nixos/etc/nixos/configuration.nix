01c8b0c5d60c773592026be9bff07982ed731cf5
Daniel Beecham <daniel@lunix.se>
2020-08-10 15:26:45 +0200

    add ssh-server to pp-ws-dbe

--- nixos/etc/nixos/configuration.nix
+++ nixos/etc/nixos/configuration.nix
@@ -95,7 +95,7 @@ in
   '';
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 24800 ];
