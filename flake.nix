e89faaad05b66e83a33ae5367fde0910bd931efb
Daniel Beecham <daniel@lunix.se>
2020-08-10 13:58:26 +0200

    move mdnsresponder out to it's own place

--- flake.nix
+++ flake.nix
@@ -37,25 +37,6 @@
         verifyCargoDeps = true;
       };
 
-      mdnsresponder = pkgs.stdenv.mkDerivation {
-        name = "mdnsresponder";
-        src = ./nixpkgs/mDNSResponder-1096.40.7.tar.gz;
-        buildPhase = ''
-          cd mDNSPosix && make os=linux
-        '';
-        installPhase = ''
-          mkdir $out;
-          mkdir $out/lib;
-          mkdir $out/bin;
-          cp build/prod/libdns_sd.so $out/lib/;
-          cp build/prod/libnss_mdns-0.2.so $out/lib/;
-          cp build/prod/mDNSClientPosix $out/bin/;
-          cp build/prod/mdnsd $out/bin/;
-          cp build/prod/mDNSNetMonitor $out/bin/;
-          cp build/prod/mDNSProxyResponderPosix $out/bin/;
-          cp build/prod/mDNSResponderPosix $out/bin/;
-        '';
-      };
     };
 
   };
